c415b31233a4c4f61bc3b8daef516d813d80e7b7
akayberk <by4berk@gmail.com>
2022-07-05 12:02:35 -0400

    bug fixed with barbarossa some comment sections added. v0.1

--- src/UnderstandingTheWhileLoop.java
+++ src/UnderstandingTheWhileLoop.java
@@ -6,7 +6,6 @@ public class UnderstandingTheWhileLoop {
         String truePassword = "123@@123";
         Scanner sc = new Scanner(System.in);
         int counter=0;
-
         while (!isTrue){
             System.out.println("Enter your password, please");
             String accuratePassword = sc.nextLine();
@@ -17,7 +16,6 @@ public class UnderstandingTheWhileLoop {
             else{
                 System.out.println("It seems that you've entered your password wrong.");
             counter++;
-
             if(counter == 3){
                 System.out.println("Would you like to change your password?     (Y/n)");
                 String answer = sc.nextLine().toLowerCase();
@@ -31,15 +29,18 @@ public class UnderstandingTheWhileLoop {
                                         "  (Y/n)");
                         String wannaTry = sc.nextLine().toLowerCase();
                         switch (wannaTry) {
+                            //Maybe we can add a checking mechanism if the old and new passwords
+                            // are the same or not. So if they are same, we don't let the user to
+                            // continue to change user password.
                             case "y":
                                 isTrue = false;
                                 break;
                             case "n":
-                                isTrue = true;
-                                break;
+                               isTrue = true;
+                                System.out.println("As you wish, dude! Adios!");
+                               break;
                         }
                         break; //kayip olan break
-
                     case "n":
                         System.out.println("As you wish, dude! Adios!");
                         isTrue = true;
