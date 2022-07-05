7f4979aa2a02b945af0076cbd131fc24871e7d89
akayberk <by4berk@gmail.com>
2022-07-05 11:21:33 -0400

    Initial commit

---
+++ src/UnderstandingTheWhileLoop.java
@@ -0,0 +1,56 @@
+import java.util.Locale;
+import java.util.Scanner;
+public class UnderstandingTheWhileLoop {
+    public static void main(String[] args) {
+        boolean isTrue = false;
+        String truePassword = "123@@123";
+        Scanner sc = new Scanner(System.in);
+        int counter=0;
+
+        while (!isTrue){
+            System.out.println("Enter your password, please");
+            String accuratePassword = sc.nextLine();
+            if (accuratePassword.contentEquals(truePassword)){
+                System.out.println("You've successfully logged-in!");
+                isTrue=true;
+            }
+            else{
+                System.out.println("It seems that you've entered your password wrong.");
+            counter++;
+
+            if(counter == 3){
+                System.out.println("Would you like to change your password?     (Y/n)");
+                String answer = sc.nextLine().toLowerCase();
+                switch (answer) {
+                    case "y":
+                        isTrue=true;
+                        System.out.println("Enter your new password");
+                        String newPassword = sc.nextLine();
+                        truePassword = newPassword;
+                        System.out.println("Would you like to try login with your new password?  " +
+                                        "  (Y/n)");
+                        String wannaTry = sc.nextLine().toLowerCase();
+                        switch (wannaTry) {
+                            case "y":
+                                isTrue = true;
+                                break;
+                            case "n":
+                                isTrue = false;
+                                break;
+                        }
+
+                    case "n":
+                        System.out.println("As you wish, dude! Adios!");
+                        isTrue = true;
+                        break;
+
+                }
+                }
+
+            }
+            }
+        }
+
+
+
+}
